a5f874e4e959058b704dceee8c0eec72ef4aa7e5
fraberret <f.berretta02@gmail.com>
2024-05-09 16:20:54 +0200

    add card in page by db

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -10,7 +10,7 @@ class PageController extends Controller
 {
     public function index()
     {
-        $movie = Movie::all();
-        return view('welcome', compact('movie'));
+        $movies = Movie::all();
+        return view('welcome', compact('movies'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Home Page</h1>
+    <div class="container">
+        <div class="row g-4">
+            @foreach ($movies as $movie)
+                <div class="col-3">
+                    <div class="card">
+                        <h3>{{ $movie['title'] }}</h3>
+                        <h5>Original title: {{ $movie['original_title'] }}</h5>
+                        <p>Nationality: {{ $movie['nationality'] }}</p>
+                        <p>Date: {{ $movie['date'] }}</p>
+                        <p>Vote: {{ $movie['vote'] }}</p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
