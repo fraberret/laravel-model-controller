40119127d42d5c21394d14ac37b4b4ea3762d80c
fraberret <f.berretta02@gmail.com>
2024-05-09 16:38:17 +0200

    add active class and style cards

--- resources/views/guests/welcome.blade.php
+++ resources/views/guests/welcome.blade.php
@@ -6,11 +6,13 @@
             @foreach ($movies as $movie)
                 <div class="col-3">
                     <div class="card">
-                        <h3>{{ $movie['title'] }}</h3>
-                        <h5>Original title: {{ $movie['original_title'] }}</h5>
-                        <p>Nationality: {{ $movie['nationality'] }}</p>
-                        <p>Date: {{ $movie['date'] }}</p>
-                        <p>Vote: {{ $movie['vote'] }}</p>
+                        <div class="card-body">
+                            <h3>{{ $movie['title'] }}</h3>
+                            <h5>Original title: {{ $movie['original_title'] }}</h5>
+                            <p>Nationality: {{ $movie['nationality'] }}</p>
+                            <p>Date: {{ $movie['date'] }}</p>
+                            <p>Vote: {{ $movie['vote'] }}</p>
+                        </div>
                     </div>
                 </div>
             @endforeach
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-sm navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="#">Movies</a>
+            <a class="navbar-brand" href="{{ route('guests.home') }}">Movies</a>
             <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
                 data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -10,14 +10,17 @@
             <div class="collapse navbar-collapse" id="collapsibleNavId">
                 <ul class="navbar-nav me-auto mt-2 mt-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" href="{{ route('guests.home') }}" aria-current="page">Home
+                        <a class="nav-link {{ Route::currentRouteName() === 'guests.home' ? 'active' : '' }}"
+                            href="{{ route('guests.home') }}" aria-current="page">Home
                             <span class="visually-hidden">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('guests.about') }}">About</a>
+                        <a class="nav-link {{ Route::currentRouteName() === 'guests.home' ? 'active' : '' }}"
+                            href="{{ route('guests.about') }}">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('guests.contacts') }}">Contacts</a>
+                        <a class="nav-link {{ Route::currentRouteName() === 'guests.home' ? 'active' : '' }}"
+                            href="{{ route('guests.contacts') }}">Contacts</a>
                     </li>
                 </ul>
                 <form class="d-flex my-2 my-lg-0">
